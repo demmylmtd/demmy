18fae6eca71ce190e3d13c9bee15f46cb182f8e9
Demmy <muhammaddemmy72@gmail.com>
2026-02-24 16:47:30 +0700

    Add Tina CMS configuration

---
+++ tina/config.ts
@@ -0,0 +1,87 @@
+import { defineConfig } from "tinacms";
+
+export default defineConfig({
+  clientId: "48326308-38d6-49f7-8c10-6483610f6d2f",
+  branch:
+    process.env.GITHUB_BRANCH ||
+    process.env.VERCEL_GIT_COMMIT_REF ||
+    process.env.HEAD ||
+    "main",
+  token: process.env.TINA_TOKEN,
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "images",
+      publicFolder: "static",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "portfolio",
+        label: "Portfolio",
+        path: "content/portfolio",
+        format: "md",
+        ui: {
+          filename: {
+            readonly: false,
+            slugify: (values) => {
+              return values?.title
+                ?.toLowerCase()
+                .replace(/ /g, "-")
+                .replace(/[^
+\w-]+/g, "");
+            },
+          },
+        },
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Date",
+          },
+          {
+            type: "boolean",
+            name: "draft",
+            label: "Draft",
+          },
+          {
+            type: "string",
+            name: "summary",
+            label: "Summary",
+            ui: {
+              component: "textarea",
+            },
+          },
+          {
+            type: "image",
+            name: "thumbnail",
+            label: "Thumbnail",
+          },
+          {
+            type: "string",
+            name: "tags",
+            label: "Tags",
+            list: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
